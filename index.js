99046b3d1b580b93fafd0081755b2d36cc616d69
PackChanHyo <cksgy1510@gmail.com>
2023-05-14 19:10:42 +0900

    env 설정

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://cksgy1510:QMKSfFhwyMhmLYC9@cluster0.awm4zxg.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB 성공"))
   .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  // 회원 가입에 필요한 정보를 client에서 가져와서 DB에 넣어준다.
+
+  const user = new User(req.body);
+
+  const reuslt = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
